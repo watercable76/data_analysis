55b83ee7ecca0ee3ba0e8e84aec7c874cc87bcf1
Nicholas Cable <cab17001@byui.edu>
2021-01-13 16:15:29 -0700

    edited ex file

--- ex_1.py
+++ ex_1.py
@@ -5,3 +5,9 @@ Purpose: Work with an example data set using matplotlib,
         and display some graphs with the data.
 Last updated: ...
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
