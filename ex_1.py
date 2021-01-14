008d8f2392d2ba313bbe954bb1b399ad759f9275
Nicholas Cable <cab17001@byui.edu>
2021-01-13 22:04:16 -0700

    Code added

--- ex_1.py
+++ ex_1.py
@@ -11,3 +11,13 @@ import numpy as np
 
 fig, ax = plt.subplots()
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+plt.plot([1, 2, 3, 4], [1, 4, 2, 3])
+
+# create an empty figure w/o aces
+fig = plt.figure()
+
+# a figure with a single axes
+fig, ax = plt.subplots()
+
+# a figure with a 2x2 grid of axes
+fig, axs = plt.subplots(2, 2)
